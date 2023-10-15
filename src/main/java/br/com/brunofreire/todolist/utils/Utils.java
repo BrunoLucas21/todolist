0aadfbab9b135d0e22eb786aa63467edc665f0ed
BrunoLucas21 <brunolucasafreire@gmail.com>
2023-10-15 17:25:28 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/brunofreire/todolist/utils/Utils.java
@@ -0,0 +1,38 @@
+package br.com.brunofreire.todolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class Utils {
+
+    public static void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+    
+    /*
+     * Pega tudo que tem de propriedade nula, ele vai atribuir para dentro do 
+     * BeanUtils.copyProperties(source, target, getNullPropertyNames(source)) e ele vai fazer a conversão para ter as mesclas das informações.
+     */
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd: pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+}
